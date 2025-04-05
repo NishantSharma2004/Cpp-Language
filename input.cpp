208b04c02c2edcd57dbc16577a53451e795e3a5a
Nishant Sharma <167605116+NishantSharma2004@users.noreply.github.com>
2025-04-05 14:41:01 +0530

    How to take input value from user

---
+++ input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int p ,q ,a,b;
+    cout<<"Enter the values of p and q = ";
+    cin>>p>>q;
+    cout<<"p ="<<p<<" , q ="<<q<<endl;
+
+    cout<<"Enter the value of a =";
+    cin>>a;
+    cout<<"a="<<a<<endl;
+    cout<<"Enter the value of b =";
+    cin>>b;
+    cout<<"b="<<b<<endl;
+    cout<<"a + b="<<a+b<<endl;
+    return 0;
+}
