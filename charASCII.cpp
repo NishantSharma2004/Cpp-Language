1df60f8d03fbfdc484dfe428aa2955d37bc42dc5
Nishant Sharma <167605116+NishantSharma2004@users.noreply.github.com>
2025-04-08 19:44:19 +0530

    Finding the ASCII value

---
+++ charASCII.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char a ='A';
+    char b ='a';
+    char c ='0';
+    cout<<(int)a<<endl; // type casting
+    cout<<(int)b<<endl;
+    cout<<(int)c<<endl;
+    return 0;
+
+}
